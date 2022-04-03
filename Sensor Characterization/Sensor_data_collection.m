a547e4cbac60dd378cd646415a89df1697411953
Miguel Loureiro <miguel.santos.loureiro@gmail.com>
2022-04-03 18:24:33 +0100

    Added script and data from sunny day.

--- Sensor Characterization/Sensor_data_collection.m
+++ Sensor Characterization/Sensor_data_collection.m
@@ -6,8 +6,21 @@ clc
 
 a = arduino("COM3", "Uno");
 
-data = zeros(500,1);
+data = zeros(5000,1);
 
 for i=1:length(data)
     data(i) = readVoltage(a, "A0");
+    pause(0.001);
 end
+
+figure
+plot(data);
+title("LDR Raw Sensor data");
+xlabel("Sample");
+ylabel("Voltage across the LDR");
+
+V = 5;
+R = 9970;
+R_ldr = R.*(data./(V-data));
+R_LDR = mean(R_ldr);
+fprintf(1,"R_LDR = %.3f Ohms", R_LDR);
